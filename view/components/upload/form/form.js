50bcc201ace25871367bb47ae50d7617c5911a0f
Arikaim <info@arikaim.com>
2022-03-07 16:34:03 +0200

    =

--- view/components/upload/form/form.js
+++ view/components/upload/form/form.js
@@ -12,18 +12,7 @@ function ImageUpload() {
 
     this.init = function() {
         arikaim.ui.form.addRules("#upload_form");
-        $('.private-image').checkbox({
-            onChecked: function() {
-                $(this).val(1);
-            },
-            onUnchecked: function() {
-                $(this).val(false);
-            }
-        });
-
-        var checked = $('.private-image').checkbox('is checked');
-        $('#private').val(checked);
-
+    
         arikaim.component.loadLibrary('filepond:preview',function(result) {      
             $.fn.filepond.registerPlugin(FilePondPluginImagePreview);
             $.fn.filepond.setDefaults({
